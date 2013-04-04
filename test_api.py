78579c5350aa8bf406d9cd2d263b5a76b68fad7f
Marcos Daniel Petry <marcos.petry@corp.globo.com>
2013-04-04 11:53:09 -0300

    refactoring test

--- test_api.py
+++ test_api.py
@@ -8,21 +8,30 @@ class ApiTestCase(unittest.TestCase):
     def setUp(self):
         self.api = api.app.test_client()
 
-    def test_should_return_201_on_create_instance(self):
-        resp = self.api.post("/resources")
-        self.assertEqual(resp.status_code, 201)
 
-    def test_should_return_201_on_bind_instance(self):
-        resp = self.api.post("/resources/my_instance")
-        self.assertEqual(resp.status_code, 201)
+
+class CreateInstanceTestCase(ApiTestCase):
+    def setUp(self):
+        super(CreateInstanceTestCase, self).setUp()
+        self.resp = self.api.post("/resources")
+
+    def test_should_return_201(self):
+        self.assertEqual(self.resp.status_code, 201)
+
+
+class BindInstanceTestCase(ApiTestCase):
+    def setUp(self):
+        super(BindInstanceTestCase, self).setUp()
+        self.resp = self.api.post("/resources/my_instance")
+
+    def test_should_return_201(self):
+        self.assertEqual(self.resp.status_code, 201)
 
     def test_should_retuns_a_json_content_type(self):
-        resp = self.api.post("/resources/my_instance")
-        self.assertEqual(resp.content_type, 'application/json')
+        self.assertEqual(self.resp.content_type, 'application/json')
 
-    def test_should_return_a_hardcoded_json_on_bind_instance(self):
-        resp = self.api.post("/resources/my_instance")
-        self.assertEqual(json.loads(resp.data) , {u'SOMEVAR': u'somevalue'})
+    def test_should_return_a_hardcoded_json(self):
+        self.assertEqual(json.loads(self.resp.data) , {u'SOMEVAR': u'somevalue'})
 
 
 
