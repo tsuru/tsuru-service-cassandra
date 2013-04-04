3f88fb581c69eb3c04bb3709374e0c1dae7ee2ec
Marcos Daniel Petry <marcos.petry@corp.globo.com>
2013-04-04 11:48:57 -0300

    added bind route

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 from flask import Flask
+from flask import jsonify
+
 app = Flask(__name__)
 
 @app.route("/resources", methods=["POST"])
 def add_instance():
     return "", 201
 
+@app.route("/resources/<name>", methods=["POST"])
+def bind(name):
+    out = jsonify(SOMEVAR="somevalue")
+    return out, 201
--- test_api.py
+++ test_api.py
@@ -1,4 +1,4 @@
-import os
+import json
 import unittest
 import api
 
@@ -11,6 +11,18 @@ class ApiTestCase(unittest.TestCase):
     def test_should_return_201_on_create_instance(self):
         resp = self.api.post("/resources")
         self.assertEqual(resp.status_code, 201)
+
+    def test_should_return_201_on_bind_instance(self):
+        resp = self.api.post("/resources/my_instance")
+        self.assertEqual(resp.status_code, 201)
+
+    def test_should_retuns_a_json_content_type(self):
+        resp = self.api.post("/resources/my_instance")
+        self.assertEqual(resp.content_type, 'application/json')
+
+    def test_should_return_a_hardcoded_json_on_bind_instance(self):
+        resp = self.api.post("/resources/my_instance")
+        self.assertEqual(json.loads(resp.data) , {u'SOMEVAR': u'somevalue'})
 
 
 
